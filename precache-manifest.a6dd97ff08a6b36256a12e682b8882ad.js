4521be46e0c159842da5c4d743ebafccb9f2858b
nishiting <xiaoruoen@163.com>
2023-04-21 17:53:07 +0800

    初始化

---
+++ precache-manifest.a6dd97ff08a6b36256a12e682b8882ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9b90310485e74c1474c",
+    "url": "./static/css/main.80b5e4be.chunk.css"
+  },
+  {
+    "revision": "e9b90310485e74c1474c",
+    "url": "./static/js/main.e9b90310.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.8c97409f.js"
+  },
+  {
+    "revision": "0b62522ea98e6a9bf1c9",
+    "url": "./static/css/2.0f20f9e6.chunk.css"
+  },
+  {
+    "revision": "0b62522ea98e6a9bf1c9",
+    "url": "./static/js/2.0b62522e.chunk.js"
+  },
+  {
+    "revision": "f2a0b2a8648aa1fbb8c174087ae75d58",
+    "url": "./index.html"
+  }
+];
